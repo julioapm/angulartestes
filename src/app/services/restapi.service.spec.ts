03561c56f1dcd45907efd222644a8142aea19c6c
Júlio Pereira Machado <julioapm@gmail.com>
2022-01-21 17:06:44 -0300

    teste ok para get

--- src/app/services/restapi.service.spec.ts
+++ src/app/services/restapi.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RestapiService } from './restapi.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Observer } from 'rxjs';
 
 //Teste unitário utilizando Jasmine e suporte dos utilitários de teste do Angular
 //Para testar chamadas HTTP utiliza-se HttpClientTestingModule e HttpTestingController
@@ -31,5 +32,23 @@ describe('RestapiService', () => {
     expect(restapiService).toBeTruthy();
   });
 
-  describe('#get', () => {});
+  describe('get', () => {
+    const dummyResponse: Dummy = {
+      name: 'test',
+      age: 10
+    };
+
+    it('should return an Observable<Dummy>', () => {
+      restapiService.get<Dummy>('/test').subscribe(data => expect(data).toEqual(dummyResponse));
+      const req = httpTestingController.expectOne('/test');
+      expect(req.request.method).toEqual('GET');
+      req.flush(dummyResponse);
+    });
+
+  });
 });
+
+interface Dummy {
+  name: string,
+  age: number
+}
